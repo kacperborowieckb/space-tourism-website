7c31a26c79cfe2995b4569e5946c025eea0e8148
kacperborowieckb <kacperborowiec.kb@gmail.com>
2023-05-19 14:05:07 +0200

    crew resp design

--- src/pages/Crew/Crew.jsx
+++ src/pages/Crew/Crew.jsx
@@ -11,14 +11,23 @@ const Crew = () => {
         <h5 className="crew__heading">
           <span className="crew__number">02</span>Meet your crew
         </h5>
-        <h4 className="crew__role">{crew[currentMember].role}</h4>
-        <h3 className="crew__name">{crew[currentMember].name}</h3>
-        <p className="crew__bio">{crew[currentMember].bio}</p>
+        <section className="crew__text-content">
+          <h4 className="crew__role">{crew[currentMember].role}</h4>
+          <h3 className="crew__name">{crew[currentMember].name}</h3>
+          <p className="crew__bio">{crew[currentMember].bio}</p>
+        </section>
+
         <section className="crew__buttons">
-          <button className="crew__button" onClick={() => setCurrentMember(0)}></button>
-          <button className="crew__button" onClick={() => setCurrentMember(1)}></button>
-          <button className="crew__button" onClick={() => setCurrentMember(2)}></button>
-          <button className="crew__button" onClick={() => setCurrentMember(3)}></button>
+          {Array(4)
+            .fill(null)
+            .map((item, i) => (
+              <button
+                key={i}
+                className="crew__button"
+                onClick={() => setCurrentMember(i)}
+                style={{ opacity: i === currentMember ? '1' : '' }}
+              ></button>
+            ))}
         </section>
       </section>
       <section className="crew__gallery">
